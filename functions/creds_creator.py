9e238b8c04d919ff8da377f5ccb59ad81ca482a7
iWanderling <slyvkanikitao@gmail.com>
2024-09-23 11:11:09 +0400

    Исправление ошибок #2

--- functions/creds_creator.py
+++ functions/creds_creator.py
@@ -1,19 +1,21 @@
 import json
+import os
 
 
 # Создание Credentials.json
 def creds_create():
+
     credentials = {
         "type": "service_account",
-        "project_id": GOOGLE_PROJECT_ID,
-        "private_key_id": GOOGLE_PROJECT_KEY_ID,
-        "private_key": GOOGLE_PRIVATE_KEY,
-        "client_email": GOOGLE_CLIENT_EMAIL,
-        "client_id": GOOGLE_CLIENT_ID,
+        "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+        "private_key_id": os.getenv("GOOGLE_PROJECT_KEY_ID"),
+        "private_key": os.getenv("GOOGLE_PRIVATE_KEY"),
+        "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/" + GOOGLE_CLIENT_CERT,
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/" + os.getenv("GOOGLE_CLIENT_CERT"),
         "universe_domain": "googleapis.com"
     }
 
--- script.py
+++ script.py
@@ -3,6 +3,7 @@ from functions.creds_creator import creds_create
 from functions.separator import separate_merges
 from functions.gsheets import gs_transfer
 from functions.parser import parsing
+from dotenv import load_dotenv
 from urllib import request
 from constant import link
 from time import sleep
@@ -10,16 +11,18 @@ import logging
 
 
 logger = logging.getLogger(__name__)
+load_dotenv()
+creds_create()
+
 
 def do_script() -> None:
 
     # Загрузка таблицы:
     request.urlretrieve(link, "tables/RawTable.xlsx")
 
-    # Программа реализуется за счёт последовательной работы четырёх функций:
+    # Программа реализуется за счёт последовательной работы трёх функций:
     separate_merges("tables/RawTable.xlsx", "tables/PreparedTable.xlsx")
     parsing("tables/PreparedTable.xlsx", "tables/CookedTable.xlsx")
-    creds_create()
     gs_transfer("tables/CookedTable.xlsx")
 
 
