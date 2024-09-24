11c98d5a12e9fad125a11faa357bb40ae78bb07a
iWanderling <slyvkanikitao@gmail.com>
2024-09-24 19:21:36 +0400

    [ALPHA 1.1] Переработаны комментарии для скрипта и функции-разделителя. Оптимизация функции-разделителя

--- functions/separator.py
+++ functions/separator.py
@@ -3,52 +3,53 @@ from constant import *
 import openpyxl
 
 
+# Получение индекса границы (левого или правого) для цикла разделения объединённых ячеек:
+def get_border_column_index(border: str) -> int:
+    if border[1].isdigit():
+        return COLUMNS.index(border[0])
+    return COLUMNS.index(border[:2])
+
+
 # Функция-разделитель объединённых в таблице ячеек.
 # Строка t_path - путь к таблице;
 # Строка dest - путь, по которому нужно сохранить обновлённую таблицу.
-def separate_merges(t_path: str, dest: str) -> None:
+def separate_merges(table_path: str, destination: str) -> None:
 
-    # Открытие и чтение таблицы
-    wb = openpyxl.open(t_path)
+    # Открытие таблицы. Создание переменной WS для чтения информации:
+    wb = openpyxl.open(table_path)
     ws = wb.active
 
-    # Список диапазонов объединённых ячеек
+    # Создаём список диапазонов объединённых ячеек:
     merged_cells = list(map(str, ws.merged_cells.ranges))
 
-    # Разделяем каждый объединённый диапазон
+    # Разделяем каждый объединённый диапазон:
     for merge in merged_cells:
         ws.unmerge_cells(range_string=merge)
 
-    # Форматируем данные о диапазонах для работы с ними
+    # Форматируем данные о диапазонах для работы с ними.
+    # Каждая строка формата A1:Z1 преобразуется в список ["A1", "Z1"]
     merged_cells = [d.split(":") for d in merged_cells]
 
-    # Разделение объединённых ячеек:
+    # Редактирование таблицы: разделение всех объединённых в таблице ячеек:
     for merge_cell in merged_cells:
 
-        # Диапазон объединённых ячеек
-        d = merge_cell
+        # Получение границ диапазона объединённых ячеек (левой и правой колонки):
+        left_col, right_col = merge_cell
 
         # Значение, которое должно быть в каждой ячейке, относящейся к диапазону
-        top_left_value = ws[d[0]].value
+        top_left_value = ws[left_col].value
 
-        # Создаём начальный флаг (вносим данные от крайней левой ячейки до крайней правой) и
-        # номер ряда, на котором будут происходить изменения:
-        if d[0][1].isdigit():
-            start = COLUMNS.index(d[0][0])
-            row = d[0][1:]
-        else:
-            start = COLUMNS.index(d[0][:2])
-            row = d[0][2:]
+        # Создаём левый и правый индексы границ:
+        start, finish = get_border_column_index(left_col), get_border_column_index(right_col)
 
-        # Создаём конечный флаг:
-        if d[1][1].isdigit():
-            finish = COLUMNS.index(d[1][0])
-        else:
-            finish = COLUMNS.index(d[1][:2])
+        # Получаем номер ряда, по которому будет пробегаться цикл:
+        row = left_col[2:]
+        if left_col[1].isdigit():
+            row = left_col[1:]
 
-        # Проходимся по каждой ячейке, вносим в неё данные
+        # Начиная от левой границы, вносим информацию в каждую ячейку ряда row, пока не дойдём до правой границы:
         for i in range(start, finish + 1):
             ws[f'{COLUMNS[i]}{row}'] = top_left_value
 
     # Сохранение таблицы:
-    wb.save(dest)
+    wb.save(destination)
--- script.py
+++ script.py
@@ -1,36 +1,38 @@
 """ Скрипт, реализующий парсинг таблицы с расписанием и перенос обновлённой таблицы в Google Sheets """
 
-# Функции программы
-from functions.creds_creator import creds_create
-from functions.separator import separate_merges
-from functions.gsheets import gs_transfer
-from functions.parser import parsing
-
-# Зависимости
-from constant import SLEEP_TIME
-from dotenv import load_dotenv
-from urllib import request
-from constant import LINKS
-from requests import get
-from flask import Flask
-from time import sleep
-import threading
-import logging
-import os
+# Основные функции программы. Импорты записаны в порядке их использования:
+from functions.creds_creator import creds_create  # Создание JSON-файла [Credentials.json]
+from functions.separator import separate_merges  # Разделение объединённых ячеек таблицы
+from functions.parser import parsing  # Парсинг таблицы
+from functions.gsheets import gs_transfer  # Загрузка таблицы с сервера в Google Sheets
 
 
-# Инициализация лога, приложения Flask, загрузка переменных сред и создание таблицы Credentials.json ("creds.json")
+# Зависимости:
+from constant import SLEEP_TIME, LINKS  # Переменная, обозначающая время перерыва и список ссылок на загрузку таблиц
+from dotenv import load_dotenv  # Загрузка переменных сред
+from urllib import request  # Загрузка таблицы
+from requests import get  # Посылка GET-запроса на сайт проекта для его непрерывной работы
+from flask import Flask  # Создание ВЕБ-сервера
+from time import sleep  # Функция для создания перерыва работы программы
+import threading  # Добавление многопоточности
+import logging  # Создание логирования
+import os  # Получение переменных сред
+
+
+# Инициализация лога, приложения Flask, загрузка переменных сред и
+# создание таблицы Credentials.json ("creds.json"):
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 load_dotenv()
 # creds_create()
 
 
-# Функция, запускающая приложение Flask:
+# Функция, запускающая приложение Flask (ВЕБ-сервер):
 def run_application():
     app.run(host="0.0.0.0", port=5000)  # port = os.getenv("PORT")
 
 
+# Главная страница сайта ВЕБ-сервера:
 @app.route('/')
 def hello_world():
     return 'Wake Up! You must work!'
@@ -38,9 +40,7 @@ def hello_world():
 
 # Функция, выполняющая скрипт - парсинг:
 def do_script() -> None:
-
     for i in range(4):
-
         # Загрузка таблицы:
         request.urlretrieve(LINKS[i], f"tables/{i + 1}/RawTable.xlsx")
 
@@ -52,15 +52,22 @@ def do_script() -> None:
 
 # Работа программы:
 if __name__ == "__main__":
+    # Создание отдельного потока для работы ВЕБ-сервера и его запуск:
     flask_thread = threading.Thread(target=run_application)
     flask_thread.start()
 
+    # Цикл, выполняющий скрипт с периодичностью SLEEP_TIME (в секундах):
     while True:
+        try:
+            logger.warning("Starting to do script...")
+            do_script()  # Выполнение скрипта
+            logger.warning("✔️ Table updates successfully... ✔️")
 
-        logger.warning("Starting to do script...")
-        do_script()
-        logger.warning("✔️ Table updates successfully... ✔️")
-        req = get("https://telegrambotkpfuscheduleparser.onrender.com")
-        logger.warning("✔️ Request has done... ✔️")
+            # Посылка GET-запроса (будим засыпающий хостинг):
+            req = get("https://telegrambotkpfuscheduleparser.onrender.com")
+            logger.warning("✔️ Request has done... ✔️")
 
-        sleep(SLEEP_TIME)
+            # Перерыв:
+            sleep(SLEEP_TIME)
+        except Exception as e:
+            logger.warning(f"❌ ERROR: {e} ❌")
